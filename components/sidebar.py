1532dc5ebcd248de769135a2c35ba3e76f7ecf18
SheilaRG26 <sheila.roca.garrido@zabalburu.org>
2025-11-07 09:22:52 +0100

    base (#1)
    Archivos y carpetas base creadas.

---
+++ components/sidebar.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+import streamlit as st
+from utils import GeminiClient, OpenAIClient
+from components import ChatInterface
+from config import DEFAULT_SETTINGS
---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .api_client import GeminiClient, OpenAIClient
+
+__all__ = ['GeminiClient', 'OpenAIClient']
