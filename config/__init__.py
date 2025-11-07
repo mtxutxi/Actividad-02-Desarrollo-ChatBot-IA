6fe56ac8e7f544997aa7d64144256debf978b86a
mtxutxi <jesusalejandro.martinez.gonzalez@zabalburu.org>
2025-11-07 09:30:46 +0100

    base
    Archivos y carpetas base creadas.

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.0.0"
+
+DEFAULT_SETTINGS = {
+    "model": "gemini-2.5-flash-lite",
+    "model_openai": "gemini-2.5-flash-lite", # Aquí estaría el modelo de OpenAI (gept-4, gpt-5...)
+    "temperature": 0.7,
+    "max_tokens": 10000
+}
